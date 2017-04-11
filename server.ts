56d7ecdf7bff492b24462edb6c8447ec8f603802
Sn0wFox <ruben.pericas@laposte.net>
2017-04-11 08:50:27 +0200

    Added full build workflow

---
+++ server.ts
@@ -0,0 +1,17 @@
+import * as express from 'express';
+import * as path    from 'path';
+
+// Configure params
+const baseFolder = path.resolve(__dirname + '/../../dist/client');
+
+// Create server app
+const app : any = express();
+
+// Configure server app
+app.set('port', process.env.PORT || 3001);  // Process port
+app.use(express.static(baseFolder));        // Serve static files
+
+// Run the server
+app.listen(app.get('port'), () => {
+  console.log('Server running on port ' + app.get('port'));
+});
