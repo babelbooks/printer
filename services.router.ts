2fdc8cd0ebb15d44aead960664012ae6169aa266
Le Poney <espeut.clement@gmail.com>
2017-04-24 14:02:04 +0200

    Fixed login form

---
+++ services.router.ts
@@ -0,0 +1,170 @@
+import * as express   from 'express';
+import * as services  from './services';
+
+export let router = express.Router();
+
+/**
+ * GET /test
+ *
+ * To test if the router is working and needed remote services.
+ * Returns a 200 status code alongside a small json object if the router is working,
+ * along with an array describing the state of the needed remotes services.
+ */
+router.get('/test', (req: express.Request, res: express.Response) => {
+  return services
+    .test()
+    .then((resp: any) => {
+      return res.status(200).json({
+        url: req.originalUrl,
+        status: 200,
+        comment: 'it\'s working!',
+        services: resp
+      });
+    })
+    .catch((err: Error) => {
+      return res.status(400).json(err);
+    });
+});
+
+/**
+ * POST / login
+ * user: {
+ *    username: ID,
+ *    password: string
+ * }
+ *
+ * Allows an user to log in, if he provides the right credentials.
+ * If so, it returns a 200 status code along with a small JSON object.
+ * Otherwise, it sends back a 400 status code.
+ */
+router.post('/login', (req: express.Request, res: express.Response) => {
+  return services
+    .login(req.body.user)
+    .then((resp: any) => {
+      return res.header('Set-Cookie', resp.headers['set-cookie'][0]).status(200).json({
+        authenticated: true
+      });
+    })
+    .catch((err: Error) => {
+      console.log(err);
+      return res.status(400).json(err);
+    });
+});
+
+/**
+ * POST /logout
+ *
+ * Allows an user to log out.
+ * If successful, it returns a 200 status code along with a small JSON object.
+ * This is not supposed to fail. If so, it therefore will send a 500 response.
+ */
+router.post('/logout', (req: express.Request, res: express.Response) => {
+  return services
+    .logout()
+    .then((resp: any) => {
+      return res.header('Set-Cookie', resp.headers['set-cookie'][0]).status(200).json({
+        authenticated: true
+      });
+    })
+    .catch((err: Error) => {
+      console.log(err);
+      return res.status(400).json(err);
+    });
+});
+
+/**
+ * GET /user/me
+ *
+ * Retrieves information about the current user,
+ * and returns it alongside a 200 status code
+ * if successful.
+ * Otherwise, returns a 400 status code along with a object
+ * describing the error.
+ */
+router.get('/user/me', (req: express.Request, res: express.Response) => {
+  return services
+    .getCurrentUser({headers: {
+      cookie: req.headers['cookie']}
+    })
+    .then((resp: any) => {
+      return res.status(200).json(resp);
+    })
+    .catch((err: Error) => {
+      return res.status(400).json(err);
+    });
+});
+
+/**
+ * GET /user/:userId/books
+ *
+ * Returns all information known about the books
+ * originally owned by the given user, alongside a 200 status code
+ * if successful.
+ * Otherwise, returns a 400 status code along with a object
+ * describing the error.
+ */
+router.get('/user/:userId/books', (req: express.Request, res: express.Response) => {
+  return services
+    .getUserLibrary(req.params['userId'], {
+      headers: {
+        cookie: req.headers['cookie']
+      }
+    })
+    .then((resp: any) => {
+      return res.status(200).json(resp);
+    })
+    .catch((err: Error) => {
+      console.log(err);
+      return res.status(400).json(err);
+    });
+});
+
+/**
+ * GET /user/:userId/books/reading
+ *
+ * Returns all information known about the books
+ * currently read by the given user, alongside a 200 status code
+ * if successful.
+ * Otherwise, returns a 400 status code along with a object
+ * describing the error.
+ */
+router.get('/user/:userId/books/reading', (req: express.Request, res: express.Response) => {
+  return services
+    .getUserReadingBooks(req.params['userId'], {
+      headers: {
+        cookie: req.headers['cookie']
+      }
+    })
+    .then((resp: any) => {
+      return res.status(200).json(resp);
+    })
+    .catch((err: Error) => {
+      console.log(err);
+      return res.status(400).json(err);
+    });
+});
+
+/**
+ * GET /user/:userId/books/borrowed
+ *
+ * Returns all information known about the books
+ * currently borrowed by the given user, alongside a 200 status code
+ * if successful.
+ * Otherwise, returns a 400 status code along with a object
+ * describing the error.
+ */
+router.get('/user/:userId/books/borrowed', (req: express.Request, res: express.Response) => {
+  return services
+    .getUserBorrowedBooks(req.params['userId'], {
+      headers: {
+        cookie: req.headers['cookie']
+      }
+    })
+    .then((resp: any) => {
+      return res.status(200).json(resp);
+    })
+    .catch((err: Error) => {
+      console.log(err);
+      return res.status(400).json(err);
+    });
+});
---
+++ services.ts
@@ -0,0 +1,123 @@
+import * as Bluebird  from 'bluebird';
+import * as request   from 'request-promise';
+
+/**
+ * Tests the status of the services that this router will use.
+ */
+export function test(): Bluebird<any> {
+  return Bluebird.all([
+    request({
+      method: 'GET',
+      url: 'http://localhost:3000/test'})
+      .catch((err: Error) => err),
+    request({
+      method: 'GET',
+      url: 'http://localhost:3002/test'})
+      .catch((err: Error) => err)
+  ]);
+}
+
+/**
+ * Tries to login the given user from
+ * the auth server.
+ * @param user The user to login.
+ * @returns {Bluebird<any>}
+ */
+export function login(user: any): Bluebird<any> {
+  return Bluebird.resolve(request({
+    method: 'POST',
+    url: 'http://localhost:3000/auth/login',
+    json: true,
+    body: user,
+    resolveWithFullResponse: true
+  }));
+}
+
+/**
+ * Logs the current user out.
+ * @returns {Bluebird<any>}
+ */
+export function logout(): Bluebird<any> {
+  return Bluebird.resolve(request({
+    method: 'POST',
+    url: 'http://localhost:3000/auth/logout',
+    json: true,
+    resolveWithFullResponse: true
+  }));
+}
+
+/**
+ * Gathers information about the currently logged-in user.
+ * @param options Request's options.
+ * @returns {Bluebird<any>}
+ */
+export function getCurrentUser(options?: any): Bluebird<any> {
+  let headers: any = options ? options.headers : undefined;
+  return Bluebird.resolve(request({
+    method: 'GET',
+    url: 'http://localhost:3000/user/me',
+    json: true,
+    headers: headers
+  }));
+}
+
+/**
+ * Gathers all information about the books originally owned
+ * by the given user.
+ * @param userId The user's Id from which retrieve the books.
+ * @param options Request's options.
+ * @returns {Bluebird<any[]>}
+ */
+export function getUserLibrary(userId: number | string, options?: any): Bluebird<any[]> {
+  return getUserBooks( 'http://localhost:3000/user/' + userId + '/books', options);
+}
+
+/**
+ * Gathers all information about the books currently borrowed
+ * by the given user.
+ * @param userId The user's Id from which retrieve the books.
+ * @param options Request's options.
+ * @returns {Bluebird<any[]>}
+ */
+export function getUserBorrowedBooks(userId: number | string, options?: any): Bluebird<any[]> {
+  return getUserBooks( 'http://localhost:3000/user/' + userId + '/books/borrowed', options);
+}
+
+/**
+ * Gathers all information about the books currently read
+ * by the given user.
+ * @param userId The user's Id from which retrieve the books.
+ * @param options Request's options.
+ * @returns {Bluebird<any[]>}
+ */
+export function getUserReadingBooks(userId: number | string, options?: any): Bluebird<any[]> {
+  return getUserBooks( 'http://localhost:3000/user/' + userId + '/books/reading', options);
+}
+
+/**
+ * A wrapper to factorize some code.
+ * @param from The url from which gather the books.
+ * @param options Request's options.
+ * @returns {Bluebird<any[]>}
+ */
+function getUserBooks(from: string, options?: any): Bluebird<any[]> {
+  let headers: any = options ? options.headers : undefined;
+  return Bluebird
+    .resolve(request({
+      method: 'GET',
+      url: from,
+      json: true,
+      headers: headers
+    }))
+    .then((res: any) => {
+      let promises: any[] = [];
+      for(let book of res.books) {
+        promises.push(request({
+          method: 'GET',
+          url: 'http://localhost:3002/book/' + book.isbn,
+          json: true
+        }))
+      }
+      return Bluebird.all(promises);
+    });
+}
